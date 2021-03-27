53ff8cdfae0bcc0333fcc8dc472cc32126381614
anandasatriaadi <putu.satriaadi@gmail.com>
2021-03-27 17:48:08 +0800

    Syntax update and some fixes

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 # 1 a
-grep -oE '(INFO\s.)|(ERROR\s.)' syslog.log > 1a.txt
+grep -oE '(INFO\s.*)|(ERROR\s.*)' syslog.log
 
 # 1 b
-grep -oE 'ERROR.*' syslog.log > 1b.txt
-echo Total Error Count = `grep -cE 'ERROR' syslog.log` >> 1b.txt
+grep -oE 'ERROR.*' syslog.log
+echo Total Error Count = $(grep -cE "ERROR" syslog.log)
 
 # 1 d
 echo "Error","Count" > error_message.csv
 errMessages=$(grep -oE 'ERROR.*' syslog.log)
-echo "$errMessages" | grep -oE "([A-Z][a-z]+)\s(['A-Za-z]+\s){1,6}" | sort | uniq |
+echo "$errMessages" | grep -oP "(?<=ERROR\s).*(?=\()" | sort | uniq |
     while read -r errMsg
     do
-        number=`grep -c "$errMsg" syslog.log`
+        number=$(grep -c "$errMsg" syslog.log)
         errMsg+=','$number
-        echo $errMsg
+        echo "$errMsg"
     done | sort -rnk 2 -t',' >> error_message.csv
 
 # 1 c&e
 echo Username,INFO,ERROR > user_statistic.csv
-users=""
 usernames=$(grep -oE '(\(.*\))' syslog.log)
 echo "$usernames" | tr -d ')' | tr -d '(' | sort | uniq |
     while read -r user
     do
         userError=0
         userInfo=0
-        for msg in `grep -wh "$user" syslog.log`
+        for msg in $(grep -wh "$user" syslog.log)
         do
             if [ "$msg" = "ERROR" ]
             then
