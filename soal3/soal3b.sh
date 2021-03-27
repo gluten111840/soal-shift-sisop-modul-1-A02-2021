80258efcf84e92cbec5d838099770fc9c95f15c2
anandasatriaadi <putu.satriaadi@gmail.com>
2021-03-27 16:43:48 +0800

    Added soal3 solutions

---
+++ soal3/soal3b.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [[ ! -d ./soal3res ]]
+then
+    mkdir ./soal3res
+fi
+
+if [[ ! -d ./soal3res/$(date +"%d-%m-%Y") ]]
+then
+    mkdir ./soal3/res$(date +"%d-%m-%Y")
+fi
+
+cd ./soal3res/$(date +"%d-%m-%Y") || exit
+
+downloadImage() {
+    if [[ $(grep -c "$web$loc" Foto.log) -gt 1 ]]
+    then
+        counter=$((counter-1))
+    else
+        wget -o - "$web$loc" > webLog.log
+    fi
+}
+
+counter=0
+for ((i=0; i<23; i=i+1))
+do
+    web="https://loremflickr.com"
+    toDownload=$(curl -s -i https://loremflickr.com/320/240/kitten)
+    loc=$(echo "$toDownload" | grep "location" | grep -oP "/cache.*" | tr -d $'\r')
+    filename=$(echo "$toDownload" | grep "location" | grep -oP "/resized/\K.*" | tr -d $'\r')
+
+    if [[ $i -eq 0 ]]
+    then
+        echo "$web$loc" > Foto.log
+        downloadImage
+    else
+        echo "$web$loc" >> Foto.log
+        downloadImage
+    fi
+
+    echo "$counter"
+    if [[ $counter -lt 10 ]]
+    then
+        mv "$filename" "Koleksi_0$counter.JPG"
+    else
+        mv "$filename" "Koleksi_$counter.JPG"
+    fi
+
+    counter=$((counter+1))
+done
+
---
+++ soal3/soal3c.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+if [[ ! -d ./soal3res ]]
+then
+    mkdir ./soal3res
+fi
+
+cd ./soal3res || exit
+
+downloadImage() {
+    if [[ $(grep -c "$web$loc" ./$folder/Foto.log) -gt 1 ]]
+    then
+        # echo "SAMA"
+        counter=$((counter-1))
+    else
+        wget -o - "$web$loc" -P "$folder" > webLog.log
+    fi
+}
+
+downloadKitten() {
+    counter=0
+    for ((i=0; i<23; i=i+1))
+    do
+        web="https://loremflickr.com"
+        toDownload=$(curl -s -i https://loremflickr.com/320/240/kitten)
+        loc=$(echo "$toDownload" | grep "location" | grep -oP "/cache.*" | tr -d $'\r')
+        filename=$(echo "$toDownload" | grep "location" | grep -oP "/resized/\K.*" | tr -d $'\r')
+
+        if [[ $i -eq 0 ]]
+        then
+            echo "$web$loc" > ./$folder/Foto.log
+            downloadImage
+        else
+            echo "$web$loc" >> ./$folder/Foto.log
+            downloadImage
+        fi
+
+        echo "$counter"
+        if [[ $counter -lt 10 ]]
+        then
+            mv "./$folder/$filename" "./$folder/Koleksi_0$counter.JPG"
+        else
+            mv "./$folder/$filename" "./$folder/Koleksi_$counter.JPG"
+        fi
+
+        counter=$((counter+1))
+    done    
+}
+
+downloadBunny() {
+    counter=0
+    for ((i=0; i<23; i=i+1))
+    do
+        web="https://loremflickr.com"
+        toDownload=$(curl -s -i https://loremflickr.com/320/240/bunny)
+        loc=$(echo "$toDownload" | grep "location" | grep -oP "/cache.*" | tr -d $'\r')
+        filename=$(echo "$toDownload" | grep "location" | grep -oP "/resized/\K.*" | tr -d $'\r')
+
+        if [[ $i -eq 0 ]]
+        then
+            echo "$web$loc" > ./$folder/Foto.log
+            downloadImage
+        else
+            echo "$web$loc" >> ./$folder/Foto.log
+            downloadImage
+        fi
+
+        echo "$counter"
+        if [[ $counter -lt 10 ]]
+        then
+            mv "./$folder/$filename" "./$folder/Koleksi_0$counter.JPG"
+        else
+            mv "./$folder/$filename" "./$folder/Koleksi_$counter.JPG"
+        fi
+
+        counter=$((counter+1))
+    done    
+}
+
+
+currentDate=$(date +"%d-%m-%Y")
+yesterDate=$(date -d "yesterday" +"%d-%m-%Y")
+
+echo "$currentDate","$yesterDate"
+
+if [[ $(ls -l | grep "$yesterDate" | grep -c "Kelinci") -eq 1 ]]
+then
+    if [[ ! -d "Kucing_$currentDate" ]]
+    then
+        mkdir "Kucing_$currentDate"
+        folder="Kucing_$currentDate"
+        downloadBunny
+    fi
+else
+    if [[ ! -d "Kelinci_$currentDate" ]]
+    then
+        mkdir "Kelinci_$currentDate"
+        folder="Kelinci_$currentDate"
+        downloadKitten
+    fi
+fi
