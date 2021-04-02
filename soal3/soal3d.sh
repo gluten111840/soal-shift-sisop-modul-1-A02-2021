836d8294e57cf570888ece67edffcbf4d94b21ea
Bayu Eka Prawira <58933506+gluten111840@users.noreply.github.com>
2021-04-02 14:37:42 +0700

    Update soal3d.sh

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -4,10 +4,8 @@ password=$(date +'%m%d%Y')
 
 cd ~/Downloads/soal-shift-sisop-modul-1-A02-2021/soal3res || exit
 
-for file in $(ls -d */);
+for file in $(ls)
 do
 zip -r -P "$password" Koleksi.zip $file;
-zip -rv Koleksi.zip "webLog.log"
 rm -rf $file
-rm -rf "webLog.log"
-done;
+done
